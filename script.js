fe3b71dd03b209e0c7c6684bf91ad23c18034f2d
user <yura.magdich@gmail.com>
2020-10-20 23:32:00 +0300

    homework 4

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
-console.log(1);
-console.log("as");
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-}
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt('How many films did you watch?', '');
+
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('How many films did you watch?', '');
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('What is your last watched film?', ''),
+                b = prompt('Rate this film');
+
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("You watched small amount of films");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("You are a casual viewer");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("You are super watcher");
+        } else {
+            console.log("Some error happened");
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <=3; i++) {
+            personalMovieDB.genres[i - 1] = prompt(`Your favourite genre is number ${i}`);
+        }
+    }
+};
